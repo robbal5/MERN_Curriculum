b1877384ca1e9d5ee1d6d27fa4d50049ee6ba9b1
Robby Balistreri <rbalistreri5@gmail.com>
2021-05-23 10:48:47 -0500

    Complete setup

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require('mongoose')
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -10,7 +13,13 @@ mongoose
 
 
 app.get("/", (req, res) => res.send("Hiya World"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
