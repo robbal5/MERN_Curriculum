228232c13cbf604c70cd4f05f07f49dfca97cb30
Robby Balistreri <rbalistreri5@gmail.com>
2021-05-23 13:57:35 -0500

    Create login JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken')
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
@@ -45,7 +47,22 @@ router.post('/login', (req, res)=> {
         bcrypt.compare(password, user.password)
         .then(isMatch => {
             if (isMatch) {
-                return res.json({message: "Success!"})
+                const payload = {
+                    id: user.id,
+                    handle: user.handle,
+                    email: user.email 
+                }
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    {expiresIn: 3600},
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: 'Bearer ' + token
+                        })
+                    }
+                )
             } else {
                 return res.status(400).json({password: "Password is not correct"})
             }
