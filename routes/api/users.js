36912f7d824490fa34997c9e2d015475943f8df0
Robby Balistreri <rbalistreri5@gmail.com>
2021-05-23 14:37:04 -0500

    Create login and registration validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,11 +3,20 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const keys = require('../../config/keys');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.post('/register', (req, res) => {
+    const {errors, isValid} = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
     // Check to make sure nobody has already registered with a duplicate email
     User.findOne({ email: req.body.email })
         .then(user => {
@@ -36,6 +45,12 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res)=> {
+
+    const {errors, isValid} = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
     const email = req.body.email;
     const password = req.body.password;
 
@@ -68,6 +83,14 @@ router.post('/login', (req, res)=> {
             }
         })
     })
+})
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        handle: req.user.handle,
+        email: req.user.email
+    });
 })
 
 module.exports = router;
