f1a38c5cc7dbefe0ac534cb679dbb04aa3a6995b
Robby Balistreri <rbalistreri5@gmail.com>
2021-05-23 22:45:41 -0500

    All code snippets included

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,12 +2,14 @@
 
 import { combineReducers } from 'redux';
 import session from './session_reducer';
-import errors from './errors_reducer'
+import errors from './errors_reducer';
+import tweets from './tweets_reducer';
 
 
 const RootReducer = combineReducers({
     session,
-    errors
+    errors,
+    tweets
 });
 
 export default RootReducer;
--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -1,18 +1,20 @@
+// src/util/session_api_util.js
+
 import axios from 'axios';
 
-export const signup = (data) => {
-    return axios.post('/api/users/register', data)
-}
-
-export const login = (data) => {
-    return axios.post('/api/users/login', data)
-}
-
+// We've been using this method in previos steps
 export const setAuthToken = token => {
-    if(token) {
-        axios.defaults.headers.common['Authorization'] = token
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = token;
     } else {
-        delete axios.defaults.headers.common['Authorization']
+        delete axios.defaults.headers.common['Authorization'];
     }
-}
+};
 
+export const signup = (userData) => {
+    return axios.post('/api/users/register', userData);
+};
+
+export const login = (userData) => {
+    return axios.post('/api/users/login', userData);
+};
