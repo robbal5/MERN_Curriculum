c8a202088f9d02f8813673493afc7e1b79f6dc6c
Robby Balistreri <rbalistreri5@gmail.com>
2021-05-23 22:08:08 -0500

    Add all provided code snippets

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,49 +1,104 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import axios from 'axios';
+// import configureStore from './store/store';
+// import {setAuthToken} from './util/session_api_util'
+// import jwt_decode from 'jwt-decode'
+// import {logout} from './actions/session_actions'
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   let store ;
+
+//   if (localStorage.jwtToken){
+//     setAuthToken(localStorage.jwtToken);
+//     const decoded = jwt_decode(localStorage.jwtToken)
+
+//     const preloadedState = {
+//       session: {
+//         isAuthenticated: true,
+//         user: decoded
+//       }
+//     }
+//     store = configureStore(preloadedState);
+//     const currentTime = Date.now/1000;
+//     if(decoded.exp < currentTime) {
+//       store.dispatch(logout())
+//     }
+//   } else {
+//     store = configureStore();
+//   }
+
+//   const root = document.getElementById('root')
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>,
+//     root
+//   );
+// })
+
+
+// window.axios = axios;
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+// src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
+
+// We will create this component shortly
+import Root from './components/root';
+
+// We set this up in the last section
 import configureStore from './store/store';
-import {setAuthToken} from './util/session_api_util'
-import jwt_decode from 'jwt-decode'
-import {logout} from './actions/session_actions'
+
+// We will use this to parse the user's session token
+import jwt_decode from 'jwt-decode';
+
+// The session utility we just created
+import { setAuthToken } from './util/session_api_util';
+
+// We have not created this action yet, but will do so in the next step
+import { logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let store ;
+  let store;
 
-  if (localStorage.jwtToken){
+  // If a returning user has a session token stored in localStorage
+  if (localStorage.jwtToken) {
+
+    // Set the token as a common header for all axios requests
     setAuthToken(localStorage.jwtToken);
-    const decoded = jwt_decode(localStorage.jwtToken)
 
-    const preloadedState = {
-      session: {
-        isAuthenticated: true,
-        user: decoded
-      }
-    }
+    // Decode the token to obtain the user's information
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+
+    // Create a preconfigured state we can immediately add to our store
+    const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+
     store = configureStore(preloadedState);
-    const currentTime = Date.now/1000;
-    if(decoded.exp < currentTime) {
-      store.dispatch(logout())
+
+    const currentTime = Date.now() / 1000;
+
+    // If the user's token has expired
+    if (decodedUser.exp < currentTime) {
+      // Logout the user and redirect to the login page
+      store.dispatch(logout());
+      window.location.href = '/login';
     }
   } else {
-    store = configureStore();
+    // If this is a first time user, start with an empty store
+    store = configureStore({});
   }
+  // Render our root component and pass in the store as a prop
+  const root = document.getElementById('root');
 
-  const root = document.getElementById('root')
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    root
-  );
-})
-
-
-window.axios = axios;
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  ReactDOM.render(<Root store={store} />, root);
+});
